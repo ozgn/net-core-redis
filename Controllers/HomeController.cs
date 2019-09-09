d55a409bd7bd8ce24da4f1b69c2682be34aa8f77
ozgn <ozgun@fixer.com.tr>
2019-09-09 15:00:08 +0300

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Redis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public HomeController (IDistributedCache distributedCache)
+        {
+            _distributedCache=distributedCache;
+        }
+        public IActionResult Index()
+        {
+            var cachedKey="Time";
+            var time=_distributedCache.GetString(cachedKey);
+            if(!string.IsNullOrEmpty(time))
+                return Content("From Cache: "+time);
+            else
+            {
+                time=DateTime.Now.ToString();
+                _distributedCache.SetString(cachedKey,time);
+                return Content("Added To Cache: "+time);
+            }
+        }
+    }
+}
